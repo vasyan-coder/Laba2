cf9c8d9528b405edaaa5dd05d074db694a7965dc
Vasyan <vasyan.coder@gmail.com>
2023-02-23 20:58:07 +0300

    feat: create UserActivity - add adjustResize argument in LoginActivity in manifest

---
+++ app/src/main/java/com/vasyancoder/laba2/UserActivity.java
@@ -0,0 +1,15 @@
+package com.vasyancoder.laba2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
