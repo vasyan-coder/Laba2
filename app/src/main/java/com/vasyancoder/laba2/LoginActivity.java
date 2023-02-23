df64b9aef7605c80bf8d2dd9cf323ca2fd3b0d78
Vasyan <vasyan.coder@gmail.com>
2023-02-24 00:44:02 +0300

    feat: add UserActivity - UserActivity get login from LoginActivity - add check input errors in LoginActivity

--- app/src/main/java/com/vasyancoder/laba2/LoginActivity.java
+++ app/src/main/java/com/vasyancoder/laba2/LoginActivity.java
@@ -1,13 +1,18 @@
 package com.vasyancoder.laba2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.inputmethod.InputMethodManager;
 
 import com.vasyancoder.laba2.databinding.ActivityLoginBinding;
 
@@ -17,30 +22,116 @@ public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
 
+
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result != null && result.getResultCode() == RESULT_OK) {
+                        if (result.getData() != null && result.getData().getStringExtra(UserActivity.KEY_NAME) != null) {
+                            binding.etPassword.setText("");                   // clear password
+                            String userLogin = result.getData().getStringExtra(UserActivity.KEY_NAME);
+                            binding.etLogin.setText(userLogin);
+                            binding.etLogin.setSelection(userLogin.length()); // position cursor at end of text
+
+                            binding.etLogin.requestFocus();                   // focus on etLogin
+                            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.showSoftInput(binding.etLogin, InputMethodManager.SHOW_IMPLICIT);
+                        }
+                    }
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        TextView title = findViewById(R.id.title);
+        //TextView title = findViewById(R.id.title);
         binding.title.setText(R.string.authorization);
 
-//        ImageView logoImage = findViewById(R.id.logoImage);
+        //ImageView logoImage = findViewById(R.id.logoImage);
         binding.logoImage.setImageResource(R.drawable.alpha_logo);
 
-//        Button signInButton = findViewById(R.id.signInButton);
+        //Button signInButton = findViewById(R.id.signInButton);
         // programmaly set click
         binding.signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "click :)");
+                checkFields();
+            }
+        });
+
+        clearListenerLoginError();
+        clearListenerPasswordError();
+    }
+
+    private void checkFields() {
+        if (binding.etLogin.getText().toString().length() != 0 &&
+                binding.etPassword.getText().toString().length() != 0) {
+
+            startForResult.launch(UserActivity.newIntentLogin(
+                    LoginActivity.this,
+                    binding.etLogin.getText().toString()));
+        } else if (binding.etLogin.getText().toString().length() == 0 &&
+                binding.etPassword.getText().toString().length() != 0) {
+
+            binding.etLoginLayout.setHelperText(getString(R.string.warning_fill));
+            binding.etLoginLayout.setHelperTextColor(getResources().getColorStateList(R.color.red));
+        } else if (binding.etPassword.getText().toString().length() == 0 &&
+                binding.etLogin.getText().toString().length() != 0) {
+
+            binding.etPasswordLayout.setHelperText(getString(R.string.warning_fill));
+            binding.etPasswordLayout.setHelperTextColor(getResources().getColorStateList(R.color.red));
+        } else {
+            binding.etPasswordLayout.setHelperText(getString(R.string.warning_fill));
+            binding.etPasswordLayout.setHelperTextColor(getResources().getColorStateList(R.color.red));
+            binding.etLoginLayout.setHelperText(getString(R.string.warning_fill));
+            binding.etLoginLayout.setHelperTextColor(getResources().getColorStateList(R.color.red));
+        }
+    }
+
+    private void clearListenerLoginError() {
+        binding.etLogin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() != 0) {
+                    binding.etLoginLayout.setHelperText("");
+                    binding.etLoginLayout.setHelperTextColor(getResources().getColorStateList(R.color.red));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
+    private void clearListenerPasswordError() {
+        binding.etPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                binding.etPasswordLayout.setHelperText("");
+                binding.etPasswordLayout.setHelperTextColor(getResources().getColorStateList(R.color.red));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
 
     // declarative set click
-//    public void signInClick(View view) {
-//        Log.d(TAG, "click :)");
-//    }
+    //public void signInClick(View view) {
+    //    Log.d(TAG, "click :)");
+    //}
 }
--- app/src/main/java/com/vasyancoder/laba2/UserActivity.java
+++ app/src/main/java/com/vasyancoder/laba2/UserActivity.java
@@ -1,15 +1,65 @@
 package com.vasyancoder.laba2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.vasyancoder.laba2.databinding.AcitivityUserBinding;
+
 public class UserActivity extends AppCompatActivity {
+
+    public static final String KEY_NAME = "user_login";
+
+    private final String TAG = "UserActivity";
+
+    private AcitivityUserBinding binding;
+    private String userLogin = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        binding = AcitivityUserBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
 
+        binding.signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultActivity();
+            }
+        });
+
+        parseIntent();
+    }
+
+    private void parseIntent() {
+        if (!getIntent().hasExtra(KEY_NAME)) {
+            throw new RuntimeException("Login doesn't input");
+        }
+        userLogin = getIntent().getStringExtra(KEY_NAME);
+        binding.welcomeTextView.setText(getString(R.string.welcome, userLogin));
+    }
+
+    @Override
+    public void onBackPressed() {
+        resultActivity();
+        super.onBackPressed();
+    }
+
+    private void resultActivity() {
+        Intent intent = new Intent();
+        intent.putExtra(KEY_NAME, userLogin);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public static Intent newIntentLogin(Context context, String userLogin) {
+        Intent intent = new Intent(context, UserActivity.class);
+        intent.putExtra(KEY_NAME, userLogin);
+        return intent;
     }
 }
