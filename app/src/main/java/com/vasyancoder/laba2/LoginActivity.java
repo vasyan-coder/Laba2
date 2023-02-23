156f5629fa16c14b7aca236b6e8a0516c8e945b1
Vasyan <vasyan.coder@gmail.com>
2023-02-23 22:53:36 +0300

    feat: add declarative and program of click

--- app/src/main/java/com/vasyancoder/laba2/LoginActivity.java
+++ app/src/main/java/com/vasyancoder/laba2/LoginActivity.java
@@ -3,10 +3,15 @@ package com.vasyancoder.laba2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,18 @@ public class LoginActivity extends AppCompatActivity {
         ImageView logoImage = findViewById(R.id.logoImage);
         logoImage.setImageResource(R.drawable.alpha_logo);
 
-
+        Button signInButton = findViewById(R.id.signInButton);
+        // programmaly set click
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "click :)");
+            }
+        });
     }
+
+    // declarative set click
+//    public void signInClick(View view) {
+//        Log.d(TAG, "click :)");
+//    }
 }
