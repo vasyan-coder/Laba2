f0801ea2a9fb35a8c685d0114b79af1b67e3cfff
Vasyan <vasyan.coder@gmail.com>
2023-02-23 22:46:06 +0300

    feat: init resources in LoginActivity

--- app/src/main/java/com/vasyancoder/laba2/LoginActivity.java
+++ app/src/main/java/com/vasyancoder/laba2/LoginActivity.java
@@ -3,6 +3,8 @@ package com.vasyancoder.laba2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView title = findViewById(R.id.title);
+        title.setText(R.string.authorization);
+
+        ImageView logoImage = findViewById(R.id.logoImage);
+        logoImage.setImageResource(R.drawable.alpha_logo);
+
+
     }
 }
