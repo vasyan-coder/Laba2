005b5fb365d99bbfa7c8774affaec9f04dff1f31
Vasily Nikitin <vasyan.coder@gmail.com>
2023-03-13 18:21:45 +0300

    feat: added adapter for HackathonsListFragment

---
+++ app/src/main/java/com/vasyancoder/laba2/model/HackathonListItem.java
@@ -0,0 +1,38 @@
+package com.vasyancoder.laba2.model;
+
+public class HackathonListItem {
+
+    private final String name;
+    private final String company;
+    private final String dateStartEnd;
+    private final String languages;
+    private final boolean status;
+
+    public HackathonListItem(String name, String company, String dateStartEnd, String languages, boolean status) {
+        this.name = name;
+        this.company = company;
+        this.dateStartEnd = dateStartEnd;
+        this.languages = languages;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getDateStartEnd() {
+        return dateStartEnd;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+}
