bdde51b846e0280e5b9caee1a8e42297742b16c4
Vasily Nikitin <vasyan.coder@gmail.com>
2023-03-13 13:57:03 +0300

    feat: added BottomNavFragment

---
+++ app/src/main/java/com/vasyancoder/laba2/BottomNavFragment.java
@@ -0,0 +1,29 @@
+package com.vasyancoder.laba2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.vasyancoder.laba2.databinding.FragmentBottomNavBinding;
+
+public class BottomNavFragment extends Fragment {
+
+    private FragmentBottomNavBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentBottomNavBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
