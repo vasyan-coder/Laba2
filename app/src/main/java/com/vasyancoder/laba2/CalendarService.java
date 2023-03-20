11d21dbbe3e22e129b84b47ceaff72fd5ad6b298
Vasily Nikitin <vasyan.coder@gmail.com>
2023-03-20 20:08:31 +0300

    feat: added service

---
+++ app/src/main/java/com/vasyancoder/laba2/CalendarService.java
@@ -0,0 +1,47 @@
+package com.vasyancoder.laba2;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+
+public class CalendarService extends Service {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        ImageButton imageButton = new ImageButton(this);
+        imageButton.setImageResource(R.mipmap.ic_launcher);
+        imageButton.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                    PixelFormat.TRANSLUCENT);
+            params.gravity = Gravity.TOP | Gravity.START;
+
+            windowManager.addView(imageButton, params);
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
